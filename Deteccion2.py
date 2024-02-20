f1c23a730c7b67c92f6be3b0e778e30ae20a54ed
pincheirapatagonia <mdpincheira@uc.cl>
2024-02-20 11:55:24 -0300

    small fix on Detection2.py

--- Deteccion2.py
+++ Deteccion2.py
@@ -1,9 +1,9 @@
-
-import os
-os.environ['NUMEXPR_MAX_THREADS'] = '12'  # Or any other number you want
-
+import math
 import cv2
 import numpy as np
+import serial
+import time
+import keyboard
 import threading
 import torch
 from torchvision import transforms
@@ -14,25 +14,30 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # Adjust fps and frame size
 out = cv2.VideoWriter('output.avi', fourcc, 1.0, (640, 480))
 
+
 class Tracker:
     def __init__(self, model):
-        
         self.record = True
         self.cap = None
-        self.image = None
-        self.out = cv2.VideoWriter('output.avi', fourcc, 1.0, (640, 480))  # Move this line inside the Tracker class
+        self.distancia = "0"
+        self.area = "0"
         self.tracking = True
         self.show = True
+        self.x = -1
+        self.y = -1
+        self.x_max = 0
+        self.y_max = 0
         self.model = model
-        self.detect = False
+        self.obj = [0, 0]
         self.camera_num = 0
-        self.video_file = "input.mp4"
-        
+
     def initiateVideo(self):
-        self.cap = cv2.VideoCapture(self.video_file)  # Modify this line
-        if not self.cap.isOpened():  # Check if the video file was successfully opened
-            print("Error opening video file")
-            return
+        print("Trying to open camera...")
+        self.cap = cv2.VideoCapture(self.camera_num)
+        while not self.cap.isOpened():
+            print("Error opening video")
+            self.cap = cv2.VideoCapture(self.camera_num)
+        print("Camera opened.")
         ret, frame = self.cap.read()
         while (not ret):
             print(ret, frame)
@@ -41,49 +46,52 @@ class Tracker:
         self.obj = [int(self.x_max / 2), int(self.y_max)]
 
     def track(self):
+        print("Tracking...")
         while self.tracking:
             # Capture frame-by-frame
             ret, frame = self.cap.read()
-            if not ret:  # Check if the frame was successfully read
-                self.tracking = False
-                break
             # Make predictions
             with torch.no_grad():
                 predictions = self.model(frame)
-                a = False
+                print(predictions)
+                min_dist = 10000000000000000000000000
+                best_x = 0
+                best_y = 0
                 for result in predictions:
-                    print(result)
                     result_bboxes = result.boxes
+                    # print(result)
                     for result_bbox in result_bboxes:
                         b_coordinates = result_bbox.xyxy[0]
+                        too_much_overlap = False
                         # get box coordinates in (top, left, bottom, right) format
                         b_center = result_bbox.xywh[0]
                         box = b_coordinates[:4].int().tolist()
-                        confidence = result_bbox.conf
-                        if confidence[0].cpu().numpy() > 0:
-                            a = True
-                            if (self.record or self.show):
-                                frame = cv2.circle(frame, (int(b_center[0].cpu().numpy()), int(
-                                    b_center[1].cpu().numpy())), 5, (0, 0, 255), -1)
-                                frame = cv2.rectangle(
-                                    frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
-                                label = "Racimo_Maduro"
-                                frame = cv2.putText(
-                                    frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                            break
-
+                       
+                        if not too_much_overlap:
+                            confidence = result_bbox.conf
+                            if confidence[0].cpu().numpy() > 0:
+                
+                                x, y, w, h = b_center.cpu().numpy()
+                                dist = (x - self.obj[0])**2 + \
+                                    (y - self.obj[1])**2
+                                if dist <= min_dist:
+                                    min_dist = dist
+                                    best_x = x
+                                    best_y = y
+                                if (self.record or self.show):
+                                    frame = cv2.circle(frame, (int(b_center[0].cpu().numpy()), int(
+                                        b_center[1].cpu().numpy())), 5, (0, 0, 255), -1)
+                                    frame = cv2.rectangle(
+                                        frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+                                    label = "Arandanos"
+                                    frame = cv2.putText(
+                                        frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                                break
+                self.x = best_x
+                self.y = best_y
                 if (self.record):
-                    frame = cv2.line(frame, (int(
-                        self.x_max / 2) - 70, int(self.y_max)), (int(self.x_max / 2) - 70, 0), (255, 0, 0), 2)
-                    frame = cv2.line(frame, (int(
-                        self.x_max / 2) + 70, int(self.y_max)), (int(self.x_max / 2) + 70, 0), (255, 0, 0), 2)
-                    self.out.write(frame)  # Use self.out instead of out
-                self.detect = a
+                    out.write(frame)
             if self.show:
-                frame = cv2.line(frame, (int(self.x_max / 2) - 70, int(self.y_max)),
-                                (int(self.x_max / 2) - 70, 0), (255, 0, 0), 2)
-                frame = cv2.line(frame, (int(self.x_max / 2) + 70, int(self.y_max)),
-                                (int(self.x_max / 2) + 70, 0), (255, 0, 0), 2)
                 cv2.imshow('Object Detection', frame)
                 cv2.waitKey(1)
 
@@ -92,48 +100,121 @@ class Tracker:
 
     def finish(self):
         self.cap.release()
-        self.out.release()  # Use self.out instead of out
         cv2.destroyAllWindows()
 
 
-class Brain:
+class Communication:
+    def __init__(self) -> None:
+        self.mostrar_contorno = False
+        self.manual_mode = False
+        self.starts = False
+        self.target_W = "COM7"
+        self.target_L = '/dev/ttyACM0'
+        self.baud = 9600
+        self.data = ''
+        self.messages = True
+        self.on = False
 
-    def __init__(self, tracker) -> None:
+    def begin(self):
+        if self.on:
+            self.device = serial.Serial(self.target_W, self.baud, timeout=1)
+            time.sleep(0.1)
+            if self.device.isOpen():
+                print("{} connected!".format(self.device.port))
+                time.sleep(1)
+
+    def read_and_print_messages(self):
+        if self.on:
+            while self.messages:
+                try:
+                    if self.device.isOpen():
+                        message = self.device.readline().decode('utf-8').strip()
+                        if message:
+                            self.data = message
+                except Exception as e:
+                    print(f"Error reading message: {e}")
+
+    def comunicacion(self, mensaje):
+        if self.on and self.device.isOpen():
+            self.device.flush()
+            self.device.write(mensaje.encode('utf-8'))
+            time.sleep(0.1)
+
+    def stop_messages(self):
+        if self.on:
+            self.messages = False
+
+
+class Brain:
+    def __init__(self, tracker, coms) -> None:
+        
         self.tracker = tracker
+        self.coms = coms
+        
+        print("Starting threads...")
         self.tracking_thread = threading.Thread(
             target=self.track_wrapper, args=())
         self.tracking_thread.daemon = True
+
+        if self.coms.on:
+            self.read_messages_thread = threading.Thread(
+                target=self.coms.read_and_print_messages)
+            self.read_messages_thread.daemon = True
+
+        
+        self.distance = 1
+        
         self.begin()
         self.do()
+
     def track_wrapper(self):
         # This function is used to run the track() method in a separate thread.
+        print("Tracking thread started...")
         self.tracker.track()
 
     def begin(self):
-        try:  # Add this line
-            print("Starting...\n")
-            self.tracker.initiateVideo()
-            print("Video Started...\n")
-            self.tracking_thread.start()
-            print("Track thread Started...\n")
-            print("Ready\n")
-            while self.tracker.detect:  # Use self.tracker.detect in a condition
-                pass  # Do something while self.tracker.detect is True
-        finally:  # Add this line
-            self.finish()
+        print("Starting...")
+        self.tracker.initiateVideo()
+        print("Video Started...")
         
+        if self.coms.on:
+            self.coms.begin()
+            print("Coms Started...")
+            self.tracking_thread.start()
+            print("Track thread Started...")
+            self.read_messages_thread.start()
+            print("read message thread Started...")
+        
+        
+    def do(self):
+        running = True
+        try:
+            while running: # Loop until the user presses 'q'
+                self.track_wrapper()        
+        except KeyboardInterrupt:
+            print("Data collection interrupted.")
+
+        finally:
+            self.finish()
+
+
     def finish(self):
         # Close the serial port
         # Release the video writer after the main loop
+        out.release()
+        if self.coms.on:
+            
+            self.coms.device.close()
+            self.coms.stop_messages()
+            self.read_messages_thread.join()
+            
         self.tracker.stop_tracking()
         self.tracker.finish()
         self.tracking_thread.join()
-        
-    def do(self):
-        return self.tracker.track()
+
 
 print("loading model...")
 
 model = YOLO("ArandanosV1.pt")
 print("model loaded")
-brain = Brain(Tracker(model))
+brain = Brain(Tracker(model), Communication())
--- Frame_extractor.py
+++ Frame_extractor.py
@@ -1,43 +1,45 @@
 import os
 import cv2
 
-def extract_frames(directory):
-    # Get a list of all video files
-    video_files = [f for f in os.listdir(directory) if f.endswith(('.mp4', '.avi', '.mov'))]
-    
+
+def extract_frames(fps, single_video, input_path):
+    # If the input is a single video file
+    if single_video:
+        video_files = [input_path]
+    else:
+        # Get a list of all video files in the directory
+        video_files = [os.path.join(input_path, f) for f in os.listdir(
+            input_path) if f.endswith(('.mp4', '.avi', '.mov'))]
+
     # For each video file
     for video_file in video_files:
         # Open the video
-        video = cv2.VideoCapture(os.path.join(directory, video_file))
-        
-        # Get the frame rate (frames per second)
-        fps = video.get(cv2.CAP_PROP_FPS)
-        
+        video = cv2.VideoCapture(video_file)
+
         # Initialize a counter for the current frame
         frame_num = 0
-        
+
         # While there are frames left in the video
         while video.isOpened():
             # Read the next frame
             ret, frame = video.read()
-            
+
             # If the frame was read correctly
             if ret:
-                # If the current frame number is a multiple of the frame rate
+                # If the current frame number is a multiple of the frame rate to extract
                 if frame_num % fps == 0:
                     # Save the frame as an image
-                    cv2.imwrite(os.path.join(directory, f'{video_file}_frame{frame_num}.jpg'), frame)
-                
+                    cv2.imwrite(f'{video_file}_frame{frame_num}.jpg', frame)
+
                 # Increment the current frame number
                 frame_num += 1
             else:
                 # If the frame was not read correctly, break the loop
                 break
-        
+
         # Release the video file
         video.release()
 
+
 # Call the function
-cwd = os.getcwd()
-target = os.path.join(cwd, 'Dataset')
-extract_frames(target)
+extract_frames(30, False, '/path/to/your/video/directory')
